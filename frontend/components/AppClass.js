5bee3f0aecf1e3b7811769c10312cbc829f7f321
Elizabeth Herrera <eaherrera098@gmail.com>
2022-04-06 00:20:04 -0400

    Fixed Class

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,38 +1,139 @@
 import React from 'react'
+import axios from 'axios'
+
+const URL = 'http://localhost:9000/api/result'
+const initialState ={
+grid: [null, null, null, null, "B", null, null, null, null],
+message : '',
+form : {
+  'x' : 2,
+  'y' : 2,
+  'steps' : 0,
+  'email' : '' 
+},
+
+}
+
 
 export default class AppClass extends React.Component {
+constructor(props) {
+  super(props)
+  this.state = initialState
+}
+updateLocation = (id) =>{
+  const form = this.state.form
+if (id === 'left' && form.x != 1) {form.x = form.x-1, form.steps = form.steps + 1,this.state.message = ''}
+ else if (id === 'right' && form.x != 3) {form.x = form.x+1, form.steps = form.steps + 1,this.state.message = '' }
+ else if (id === 'up' && form.y != 1) {form.y = form.y-1, this.state.form.steps = this.state.form.steps + 1, this.state.message = ''}
+ else if (id === 'down' && form.y != 3) {form.y = form.y+1, form.steps = form.steps + 1,this.state.message = ''}
+ else if (id === 'reset'){form.x = 2, form.y = 2, form.steps = 0, this.state.message = ''}
+ else if (id === 'right' && form.x === 3) {this.state.message = "You can't go right"}
+ else if (id === 'left' && form.x === 1){this.state.message = "You can't go left"}
+ else if (id === 'up' && form.y === 1) {this.state.message = "You can't go up"}
+ else if (id === 'down' && form.y === 3){this.state.message = "You can't go down"}
+ this.setState({...this.state,form: form, grid:this.gridSwitch(form.x, form.y)})
+ 
+
+}
+
+gridSwitch = (x, y) => {
+
+ if(x ===1 && y === 1 ){return ["B", null, null, null, null, null, null, null, null]}
+ else if (x === 2 && y === 1){return[null, "B", null, null, null, null, null, null, null]}
+ else if (x === 3 && y === 1){return[null, null, "B", null, null, null, null, null, null]}
+ else if (x === 1 && y === 2){return[null, null, null, "B", null, null, null, null, null]}
+ else if(x === 2 && y === 2){return[null, null, null, null, "B", null, null, null, null]}
+ else if(x === 3&& y === 2){return[null, null, null, null, null, "B", null, null, null]}
+ else if(x === 1 && y === 3){return[null, null, null, null, null , null,"B", null, null]}
+ else if(x === 2 && y === 3){return[null, null, null, null, null , null, null,"B", null]}
+ else if(x === 3 && y === 3){return[null, null, null, null, null, null, null, null, "B"]}
+
+
+
+}
+
+onChange = (key, value) => {
+this.setState({
+  ...this.state,
+  form: {...this.state.form, [key]: value},
+})
+}
+updateForm = (evt) => {
+const name = evt.target.name;
+const value = evt.target.value;
+this.onChange(name, value)
+}
+
+onSubmit = (evt) => {
+  evt.preventDefault()
+  this.addNew()
+  this.setState({
+    ...this.state,
+    form: {...this.state.form, ['email']: ''},
+  })
+}
+addNew = () =>{
+  const newForm = {
+    x: this.state.form.x,
+    y : this.state.form.y,
+    steps : this.state.form.steps,
+    email: this.state.form.email,
+  }
+  console.log(newForm)
+axios.post(URL, newForm)
+.then(res => {
+  console.log(res)
+  this.setState({
+    ...this.state,
+    message: [...this.state.message, res.data.message]
+  })
+})
+.catch(err => {
+  this.setState({
+    ...this.state,
+    message: err.response.data.message,
+  })
+})
+}
+
+
+
+
+
+
   render() {
     const { className } = this.props
+    
     return (
       <div id="wrapper" className={className}>
         <div className="info">
-          <h3 id="coordinates">Coordinates (2, 2)</h3>
-          <h3 id="steps">You moved 0 times</h3>
+          <h3 id="coordinates">({this.state.form.x}, {this.state.form.y})</h3>
+          <h3 id="steps">You moved {this.state.form.steps} {`${this.state.form.steps === 1 ? 'time' : 'times'}`}</h3>
         </div>
         <div id="grid">
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square active">B</div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
+          <div className={`square${this.state.grid[0]? ' active': ''}`}>{this.state.grid[0]}</div>
+          <div className={`square${this.state.grid[1]? ' active': ''}`}>{this.state.grid[1]}</div>
+          <div className={`square${this.state.grid[2]? ' active': ''}`}>{this.state.grid[2]}</div>
+          <div className={`square${this.state.grid[3]? ' active': ''}`}>{this.state.grid[3]}</div>
+          <div className={`square${this.state.grid[4]? ' active': ''}`}>{this.state.grid[4]}</div>
+          <div className={`square${this.state.grid[5]? ' active': ''}`}>{this.state.grid[5]}</div>
+          <div className={`square${this.state.grid[6]? ' active': ''}`}>{this.state.grid[6]}</div>
+          <div className={`square${this.state.grid[7]? ' active': ''}`}>{this.state.grid[7]}</div>
+          <div className={`square${this.state.grid[8]? ' active': ''}`}>{this.state.grid[8]}</div>
         </div>
         <div className="info">
-          <h3 id="message"></h3>
+          <h3 id="message">{this.state.message}</h3>
         </div>
         <div id="keypad">
-          <button id="left">LEFT</button>
-          <button id="up">UP</button>
-          <button id="right">RIGHT</button>
-          <button id="down">DOWN</button>
-          <button id="reset">reset</button>
+          <button onClick={evt => this.updateLocation('left')} id="left">LEFT</button>
+          <button onClick={evt => this.updateLocation("up")} id="up">UP</button>
+          <button onClick={evt => this.updateLocation('right')} id="right">RIGHT</button>
+          <button onClick={evt => this.updateLocation('down')} id="down">DOWN</button>
+          <button onClick={evt => this.updateLocation('reset')}id="reset">reset</button>
         </div>
         <form>
-          <input id="email" type="email" placeholder="type email"></input>
-          <input id="submit" type="submit"></input>
+          <input onChange={evt => this.updateForm(evt)} id="email" name = "email" value = {this.state.form.email} type="email" placeholder="type email"></input>
+          <input id="submit" type="submit" onClick = {evt => this.onSubmit(evt)}></input>
         </form>
       </div>
     )
--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,37 +1,174 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
+
+const URL = "http://localhost:9000/api/result";
+const initialState = {
+  grid: [null, null, null, null, "B", null, null, null, null],
+  message: "",
+  form: {
+    x: 2,
+    y: 2,
+    steps: 0,
+    email: "",
+  },
+};
 
 export default function AppFunctional(props) {
+  const [grid, setGrid] = useState([
+    null,
+    null,
+    null,
+    null,
+    "B",
+    null,
+    null,
+    null,
+    null,
+  ]);
+  const [message, setMessage] = useState("");
+  const [x, setX] = useState(2);
+  const [y, setY] = useState(2);
+  const [steps, setSteps] = useState(0);
+  const [email, setEmail] = useState("");
+
+  function updateLocation(id) {
+ 
+    if (id === "left" && x != 1) {
+      setX(x - 1), setSteps(steps + 1), setMessage(""),setGrid(gridSwitch(x, y))
+    } else if (id === "right" && x != 3) {
+      setX(x + 1), setSteps(steps + 1), setMessage(""),setGrid(gridSwitch(x, y))
+    } else if (id === "up" && y != 1) {
+      setY(y - 1), setSteps(steps + 1), setMessage(""),setGrid(gridSwitch(x, y))
+    } else if (id === "down" && y != 3) {
+      setY(y + 1), setSteps(steps + 1), setMessage(""),setGrid(gridSwitch(x, y))
+    } else if (id === "reset") {
+      setX(2), setY(2), setSteps(0), setMessage(""),setGrid(gridSwitch(x, y))
+    } else if (id === "right" && x === 3) {
+      setMessage("You can't go right");
+    } else if (id === "left" && x === 1) {
+      setMessage("You can't go left");
+    } else if (id === "up" && y === 1) {
+      setMessage("You can't go up");
+    } else if (id === "down" && y === 3) {
+      setMessage("You can't go down");
+    }
+    setGrid(gridSwitch(x, y))
+    console.log(grid);
+  }
+
+  function gridSwitch(x, y) {
+    if (x === 1 && y === 1) {
+      return(["B", null, null, null, null, null, null, null, null]);
+    } else if (x === 2 && y === 1) {
+      return([null, "B", null, null, null, null, null, null, null]);
+    } else if (x === 3 && y === 1) {
+      return([null, null, "B", null, null, null, null, null, null]);
+    } else if (x === 1 && y === 2) {
+      return([null, null, null, "B", null, null, null, null, null]);
+    } else if (x === 2 && y === 2) {
+      return([null, null, null, null, "B", null, null, null, null]);
+    } else if (x === 3 && y === 2) {
+      return([null, null, null, null, null, "B", null, null, null]);
+    } else if (x === 1 && y === 3) {
+      return([null, null, null, null, null, null, "B", null, null]);
+    } else if (x === 2 && y === 3) {
+      return([null, null, null, null, null, null, null, "B", null]);
+    } else if (x === 3 && y === 3) {
+      return([null, null, null, null, null, null, null, null, "B"]);
+    }
+  }
+  
+
+  function onChange(key, value) {
+    this.setState({
+      ...this.state,
+      form: {...this.state.form, [key]: value},
+    })
+    }
+    function updateForm(evt) {
+    const name = evt.target.name;
+    const value = evt.target.value;
+    this.onChange(name, value)
+    }
+    
+    function onSubmit(evt) {
+      evt.preventDefault()
+      this.addNew()
+      this.setState({
+        ...this.state,
+        form: {...this.state.form, ['email']: ''},
+      })
+    }
+    function addNew(){
+      const newForm = {
+        x: x,
+        y :y,
+        steps : steps,
+        email: email,
+      }
+      console.log(newForm)
+    axios.post(URL, newForm)
+    .then(res => {
+      console.log(res)
+      this.setState({
+        ...this.state,
+        message: [...this.state.message, res.data.message]
+      })
+    })
+    .catch(err => {
+      this.setState({
+        ...this.state,
+        message: err.response.data.message,
+      })
+    })
+    }
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">Coordinates (2, 2)</h3>
-        <h3 id="steps">You moved 0 times</h3>
+        <h3 id="coordinates">
+          Coordinates ({x},{y} )
+        </h3>
+        <h3 id="steps">
+          You moved {steps} {`${steps === 1 ? "time" : "times"}`}
+        </h3>
       </div>
       <div id="grid">
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square active">B</div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
+        <div className={`square${grid[0] ? " active" : ""}`}>{grid[0]}</div>
+        <div className={`square${grid[1] ? " active" : ""}`}>{grid[1]}</div>
+        <div className={`square${grid[2] ? " active" : ""}`}>{grid[2]}</div>
+        <div className={`square${grid[3] ? " active" : ""}`}>{grid[3]}</div>
+        <div className={`square${grid[4] ? " active" : ""}`}>{grid[4]}</div>
+        <div className={`square${grid[5] ? " active" : ""}`}>{grid[5]}</div>
+        <div className={`square${grid[6] ? " active" : ""}`}>{grid[6]}</div>
+        <div className={`square${grid[7] ? " active" : ""}`}>{grid[7]}</div>
+        <div className={`square${grid[8] ? " active" : ""}`}>{grid[8]}</div>
       </div>
       <div className="info">
-        <h3 id="message"></h3>
+        <h3 id="message">{message}</h3>
       </div>
       <div id="keypad">
-        <button id="left">LEFT</button>
-        <button id="up">UP</button>
-        <button id="right">RIGHT</button>
-        <button id="down">DOWN</button>
-        <button id="reset">reset</button>
+        <button onClick={(evt) => updateLocation("left")} id="left">
+          LEFT
+        </button>
+        <button onClick={(evt) => updateLocation("up")} id="up">
+          UP
+        </button>
+        <button onClick={(evt) => updateLocation("right")} id="right">
+          RIGHT
+        </button>
+        <button onClick={(evt) => updateLocation("down")} id="down">
+          DOWN
+        </button>
+        <button onClick={(evt) => updateLocation("reset")} id="reset">
+          reset
+        </button>
       </div>
       <form>
         <input id="email" type="email" placeholder="type email"></input>
         <input id="submit" type="submit"></input>
       </form>
     </div>
-  )
+  );
 }
+
+
